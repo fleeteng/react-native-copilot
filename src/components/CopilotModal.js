0ef0e9818d213025a503b2b5045284f8d1da6208
tankers746 <tankers746@live.com>
2020-03-29 01:00:10 +0800

    Allow touches through window, inject progress function into wrapped components

--- src/components/CopilotModal.js
+++ src/components/CopilotModal.js
@@ -1,10 +1,25 @@
 // @flow
-import React, { Component } from 'react';
-import { Animated, Easing, View, NativeModules, Modal, StatusBar, Platform } from 'react-native';
-import Tooltip from './Tooltip';
-import StepNumber from './StepNumber';
-import styles, { MARGIN, ARROW_SIZE, STEP_NUMBER_DIAMETER, STEP_NUMBER_RADIUS } from './style';
-import type { SvgMaskPathFn } from '../types';
+import React, { Component } from "react";
+import {
+  Animated,
+  Easing,
+  View,
+  NativeModules,
+  Modal,
+  StatusBar,
+  Platform,
+  PanResponder,
+  Alert
+} from "react-native";
+import Tooltip from "./Tooltip";
+import StepNumber from "./StepNumber";
+import styles, {
+  MARGIN,
+  ARROW_SIZE,
+  STEP_NUMBER_DIAMETER,
+  STEP_NUMBER_RADIUS
+} from "./style";
+import type { SvgMaskPathFn } from "../types";
 
 type Props = {
   stop: () => void,
@@ -20,13 +35,13 @@ type Props = {
   tooltipComponent: ?React$Component,
   tooltipStyle?: Object,
   stepNumberComponent: ?React$Component,
-  overlay: 'svg' | 'view',
+  overlay: "svg" | "view",
   animated: boolean,
   androidStatusBarVisible: boolean,
   backdropColor: string,
   labels: Object,
   svgMaskPath?: SvgMaskPathFn,
-  stopOnOutsideClick?: boolean,
+  stopOnOutsideClick?: boolean
 };
 
 type State = {
@@ -36,8 +51,8 @@ type State = {
   notAnimated: boolean,
   layout: ?{
     width: number,
-    height: number,
-  },
+    height: number
+  }
 };
 
 const noop = () => {};
@@ -50,13 +65,14 @@ class CopilotModal extends Component<Props, State> {
     tooltipStyle: {},
     stepNumberComponent: StepNumber,
     // If react-native-svg native module was avaialble, use svg as the default overlay component
-    overlay: typeof NativeModules.RNSVGSvgViewManager !== 'undefined' ? 'svg' : 'view',
+    overlay:
+      typeof NativeModules.RNSVGSvgViewManager !== "undefined" ? "svg" : "view",
     // If animated was not specified, rely on the default overlay type
-    animated: typeof NativeModules.RNSVGSvgViewManager !== 'undefined',
+    animated: typeof NativeModules.RNSVGSvgViewManager !== "undefined",
     androidStatusBarVisible: false,
-    backdropColor: 'rgba(0, 0, 0, 0.4)',
+    backdropColor: "rgba(0, 0, 0, 0.4)",
     labels: {},
-    stopOnOutsideClick: false,
+    stopOnOutsideClick: false
   };
 
   state = {
@@ -64,10 +80,10 @@ class CopilotModal extends Component<Props, State> {
     arrow: {},
     animatedValues: {
       top: new Animated.Value(0),
-      stepNumberLeft: new Animated.Value(0),
+      stepNumberLeft: new Animated.Value(0)
     },
     animated: false,
-    containerVisible: false,
+    containerVisible: false
   };
 
   componentDidUpdate(prevProps: Props) {
@@ -78,22 +94,27 @@ class CopilotModal extends Component<Props, State> {
 
   layout = {
     width: 0,
-    height: 0,
-  }
+    height: 0
+  };
 
   handleLayoutChange = ({ nativeEvent: { layout } }) => {
     this.layout = layout;
-  }
+  };
 
   measure(): Promise {
-    if (typeof __TEST__ !== 'undefined' && __TEST__) { // eslint-disable-line no-undef
-      return new Promise(resolve => resolve({
-        x: 0, y: 0, width: 0, height: 0,
-      }));
+    if (typeof __TEST__ !== "undefined" && __TEST__) {
+      // eslint-disable-line no-undef
+      return new Promise(resolve =>
+        resolve({
+          x: 0,
+          y: 0,
+          width: 0,
+          height: 0
+        })
+      );
     }
 
-
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       const setLayout = () => {
         if (this.layout.width !== 0) {
           resolve(this.layout);
@@ -107,22 +128,22 @@ class CopilotModal extends Component<Props, State> {
 
   async _animateMove(obj = {}): void {
     const layout = await this.measure();
-    if (!this.props.androidStatusBarVisible && Platform.OS === 'android') {
+    if (!this.props.androidStatusBarVisible && Platform.OS === "android") {
       obj.top -= StatusBar.currentHeight; // eslint-disable-line no-param-reassign
     }
 
     let stepNumberLeft = obj.left - STEP_NUMBER_RADIUS;
 
     if (stepNumberLeft < 0) {
-      stepNumberLeft = (obj.left + obj.width) - STEP_NUMBER_RADIUS;
+      stepNumberLeft = obj.left + obj.width - STEP_NUMBER_RADIUS;
       if (stepNumberLeft > layout.width - STEP_NUMBER_DIAMETER) {
         stepNumberLeft = layout.width - STEP_NUMBER_DIAMETER;
       }
     }
 
     const center = {
-      x: obj.left + (obj.width / 2),
-      y: obj.top + (obj.height / 2),
+      x: obj.left + obj.width / 2,
+      y: obj.top + obj.height / 2
     };
 
     const relativeToLeft = center.x;
@@ -130,25 +151,28 @@ class CopilotModal extends Component<Props, State> {
     const relativeToBottom = Math.abs(center.y - layout.height);
     const relativeToRight = Math.abs(center.x - layout.width);
 
-    const verticalPosition = relativeToBottom > relativeToTop ? 'bottom' : 'top';
-    const horizontalPosition = relativeToLeft > relativeToRight ? 'left' : 'right';
+    const verticalPosition =
+      relativeToBottom > relativeToTop ? "bottom" : "top";
+    const horizontalPosition =
+      relativeToLeft > relativeToRight ? "left" : "right";
 
     const tooltip = {};
     const arrow = {};
 
-    if (verticalPosition === 'bottom') {
+    if (verticalPosition === "bottom") {
       tooltip.top = obj.top + obj.height + MARGIN;
-      arrow.borderBottomColor = '#fff';
-      arrow.top = tooltip.top - (ARROW_SIZE * 2);
+      arrow.borderBottomColor = "#fff";
+      arrow.top = tooltip.top - ARROW_SIZE * 2;
     } else {
       tooltip.bottom = layout.height - (obj.top - MARGIN);
-      arrow.borderTopColor = '#fff';
-      arrow.bottom = tooltip.bottom - (ARROW_SIZE * 2);
+      arrow.borderTopColor = "#fff";
+      arrow.bottom = tooltip.bottom - ARROW_SIZE * 2;
     }
 
-    if (horizontalPosition === 'left') {
+    if (horizontalPosition === "left") {
       tooltip.right = Math.max(layout.width - (obj.left + obj.width), 0);
-      tooltip.right = tooltip.right === 0 ? tooltip.right + MARGIN : tooltip.right;
+      tooltip.right =
+        tooltip.right === 0 ? tooltip.right + MARGIN : tooltip.right;
       tooltip.maxWidth = layout.width - tooltip.right - MARGIN;
       arrow.right = tooltip.right + MARGIN;
     } else {
@@ -160,20 +184,21 @@ class CopilotModal extends Component<Props, State> {
 
     const animate = {
       top: obj.top,
-      stepNumberLeft,
+      stepNumberLeft
     };
 
     if (this.state.animated) {
-      Animated
-        .parallel(Object.keys(animate)
-          .map(key => Animated.timing(this.state.animatedValues[key], {
+      Animated.parallel(
+        Object.keys(animate).map(key =>
+          Animated.timing(this.state.animatedValues[key], {
             toValue: animate[key],
             duration: this.props.animationDuration,
-            easing: this.props.easing,
-          })))
-        .start();
+            easing: this.props.easing
+          })
+        )
+      ).start();
     } else {
-      Object.keys(animate).forEach((key) => {
+      Object.keys(animate).forEach(key => {
         this.state.animatedValues[key].setValue(animate[key]);
       });
     }
@@ -185,23 +210,22 @@ class CopilotModal extends Component<Props, State> {
       animated: this.props.animated,
       size: {
         x: obj.width,
-        y: obj.height,
+        y: obj.height
       },
       position: {
         x: Math.floor(Math.max(obj.left, 0)),
-        y: Math.floor(Math.max(obj.top, 0)),
-      },
+        y: Math.floor(Math.max(obj.top, 0))
+      }
     });
   }
 
   animateMove(obj = {}): void {
-    return new Promise((resolve) => {
-      this.setState(
-        { containerVisible: true },
-        () => requestAnimationFrame(async () => {
+    return new Promise(resolve => {
+      this.setState({ containerVisible: true }, () =>
+        requestAnimationFrame(async () => {
           await this._animateMove(obj);
           resolve();
-        }),
+        })
       );
     });
   }
@@ -210,22 +234,22 @@ class CopilotModal extends Component<Props, State> {
     this.setState({
       animated: false,
       containerVisible: false,
-      layout: undefined,
+      layout: undefined
     });
   }
 
   handleNext = () => {
     this.props.next();
-  }
+  };
 
   handlePrev = () => {
     this.props.prev();
-  }
+  };
 
   handleStop = () => {
     this.reset();
     this.props.stop();
-  }
+  };
 
   handleMaskClick = () => {
     if (this.props.stopOnOutsideClick) {
@@ -235,9 +259,10 @@ class CopilotModal extends Component<Props, State> {
 
   renderMask() {
     /* eslint-disable global-require */
-    const MaskComponent = this.props.overlay === 'svg'
-      ? require('./SvgMask').default
-      : require('./ViewMask').default;
+    const MaskComponent =
+      this.props.overlay === "svg"
+        ? require("./SvgMask").default
+        : require("./ViewMask").default;
     /* eslint-enable */
     return (
       <MaskComponent
@@ -258,7 +283,7 @@ class CopilotModal extends Component<Props, State> {
   renderTooltip() {
     const {
       tooltipComponent: TooltipComponent,
-      stepNumberComponent: StepNumberComponent,
+      stepNumberComponent: StepNumberComponent
     } = this.props;
 
     return [
@@ -268,8 +293,11 @@ class CopilotModal extends Component<Props, State> {
           styles.stepNumberContainer,
           {
             left: this.state.animatedValues.stepNumberLeft,
-            top: Animated.add(this.state.animatedValues.top, -STEP_NUMBER_RADIUS),
-          },
+            top: Animated.add(
+              this.state.animatedValues.top,
+              -STEP_NUMBER_RADIUS
+            )
+          }
         ]}
       >
         <StepNumberComponent
@@ -280,7 +308,10 @@ class CopilotModal extends Component<Props, State> {
         />
       </Animated.View>,
       <Animated.View key="arrow" style={[styles.arrow, this.state.arrow]} />,
-      <Animated.View key="tooltip" style={[styles.tooltip, this.props.tooltipStyle, this.state.tooltip]}>
+      <Animated.View
+        key="tooltip"
+        style={[styles.tooltip, this.props.tooltipStyle, this.state.tooltip]}
+      >
         <TooltipComponent
           isFirstStep={this.props.isFirstStep}
           isLastStep={this.props.isLastStep}
@@ -290,7 +321,7 @@ class CopilotModal extends Component<Props, State> {
           handleStop={this.handleStop}
           labels={this.props.labels}
         />
-      </Animated.View>,
+      </Animated.View>
     ];
   }
 
@@ -299,21 +330,29 @@ class CopilotModal extends Component<Props, State> {
     const contentVisible = this.state.layout && containerVisible;
 
     return (
-      <Modal
+      <View
+        pointerEvents="none"
+        style={{
+          zIndex: 999,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          flex: 1,
+          opacity: containerVisible ? 1 : 0
+        }}
         animationType="none"
         visible={containerVisible}
         onRequestClose={noop}
         transparent
-        supportedOrientations={['portrait', 'landscape']}
+        supportedOrientations={["portrait", "landscape"]}
       >
-        <View
-          style={styles.container}
-          onLayout={this.handleLayoutChange}
-        >
+        <View style={styles.container} onLayout={this.handleLayoutChange}>
           {contentVisible && this.renderMask()}
           {contentVisible && this.renderTooltip()}
         </View>
-      </Modal>
+      </View>
     );
   }
 }
--- src/hocs/copilot.js
+++ src/hocs/copilot.js
@@ -1,18 +1,24 @@
 // @flow
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import { findNodeHandle, View } from 'react-native';
+import { findNodeHandle, View, PanResponder, Alert } from "react-native";
 
-import mitt from 'mitt';
-import hoistStatics from 'hoist-non-react-statics';
+import mitt from "mitt";
+import hoistStatics from "hoist-non-react-statics";
 
-import CopilotModal from '../components/CopilotModal';
-import { OFFSET_WIDTH } from '../components/style';
+import CopilotModal from "../components/CopilotModal";
+import { OFFSET_WIDTH } from "../components/style";
 
-import { getFirstStep, getLastStep, getStepNumber, getPrevStep, getNextStep } from '../utilities';
+import {
+  getFirstStep,
+  getLastStep,
+  getStepNumber,
+  getPrevStep,
+  getNextStep
+} from "../utilities";
 
-import type { Step, CopilotContext } from '../types';
+import type { Step, CopilotContext } from "../types";
 
 /*
 This is the maximum wait time for the steps to be registered before starting the tutorial
@@ -27,7 +33,7 @@ type State = {
   androidStatusBarVisible: boolean,
   backdropColor: string,
   scrollView?: React.RefObject,
-  stopOnOutsideClick?: boolean,
+  stopOnOutsideClick?: boolean
 };
 
 const copilot = ({
@@ -42,194 +48,235 @@ const copilot = ({
   stopOnOutsideClick = false,
   svgMaskPath,
   verticalOffset = 0,
-  wrapperStyle,
-} = {}) =>
-  (WrappedComponent) => {
-    class Copilot extends Component<any, State> {
-      state = {
-        steps: {},
-        currentStep: null,
-        visible: false,
-        scrollView: null,
-      };
+  wrapperStyle
+} = {}) => WrappedComponent => {
+  class Copilot extends Component<any, State> {
+    state = {
+      steps: {},
+      currentStep: null,
+      visible: false,
+      scrollView: null
+    };
 
-      getChildContext(): { _copilot: CopilotContext } {
-        return {
-          _copilot: {
-            registerStep: this.registerStep,
-            unregisterStep: this.unregisterStep,
-            getCurrentStep: () => this.state.currentStep,
-          },
-        };
+    enableTouchesInTarget = ({ nativeEvent }) => {
+      if (!this.size || !this.layout) {
+        return false;
       }
+      const touchX = nativeEvent.pageX;
+      const touchY = nativeEvent.pageY;
 
-      componentDidMount() {
-        this.mounted = true;
+      const insideWidth =
+        this.size.x < touchX && touchX < this.size.x + this.size.width;
+      const insideHeight =
+        this.size.y < touchY && touchY < this.size.y + this.size.height;
+      return !insideWidth || !insideHeight;
+    };
+
+    panResponder = PanResponder.create({
+      onMoveShouldSetPanResponderCapture: this.enableTouchesInTarget,
+      onStartShouldSetPanResponderCapture: this.enableTouchesInTarget,
+      onPanResponderTerminationRequest: () => false
+    });
+
+    handleLayoutChange = ({ nativeEvent: { layout } }) => {
+      this.layout = layout;
+    };
+
+    componentDidMount() {
+      this.mounted = true;
+    }
+
+    componentWillUnmount() {
+      this.mounted = false;
+    }
+
+    getStepNumber = (step: ?Step = this.state.currentStep): number =>
+      getStepNumber(this.state.steps, step);
+
+    getFirstStep = (): ?Step => getFirstStep(this.state.steps);
+
+    getLastStep = (): ?Step => getLastStep(this.state.steps);
+
+    getPrevStep = (step: ?Step = this.state.currentStep): ?Step =>
+      getPrevStep(this.state.steps, step);
+
+    getNextStep = (step: ?Step = this.state.currentStep): ?Step =>
+      getNextStep(this.state.steps, step);
+
+    setCurrentStep = async (step: Step, move?: boolean = true): void => {
+      await this.setState({ currentStep: step });
+      this.eventEmitter.emit("stepChange", step);
+
+      if (this.state.scrollView) {
+        const { scrollView } = this.state;
+        await this.state.currentStep.wrapper.measureLayout(
+          findNodeHandle(scrollView),
+          (x, y, w, h) => {
+            const yOffsett = y > 0 ? y - h / 2 : 0;
+            scrollView.scrollTo({ y: yOffsett, animated: false });
+          }
+        );
       }
-
-      componentWillUnmount() {
-        this.mounted = false;
-      }
-
-      getStepNumber = (step: ?Step = this.state.currentStep): number =>
-        getStepNumber(this.state.steps, step);
-
-      getFirstStep = (): ?Step => getFirstStep(this.state.steps);
-
-      getLastStep = (): ?Step => getLastStep(this.state.steps);
-
-      getPrevStep = (step: ?Step = this.state.currentStep): ?Step =>
-        getPrevStep(this.state.steps, step);
-
-      getNextStep = (step: ?Step = this.state.currentStep): ?Step =>
-        getNextStep(this.state.steps, step);
-
-      setCurrentStep = async (step: Step, move?: boolean = true): void => {
-        await this.setState({ currentStep: step });
-        this.eventEmitter.emit('stepChange', step);
-
-        if (this.state.scrollView) {
-          const { scrollView } = this.state;
-          await this.state.currentStep.wrapper.measureLayout(
-            findNodeHandle(scrollView), (x, y, w, h) => {
-              const yOffsett = y > 0 ? y - (h / 2) : 0;
-              scrollView.scrollTo({ y: yOffsett, animated: false });
-            });
-        }
-        setTimeout(() => {
+      setTimeout(
+        () => {
           if (move) {
             this.moveToCurrentStep();
           }
-        }, this.state.scrollView ? 100 : 0);
-      }
+        },
+        this.state.scrollView ? 100 : 0
+      );
+    };
 
-      setVisibility = (visible: boolean): void => new Promise((resolve) => {
+    setVisibility = (visible: boolean): void =>
+      new Promise(resolve => {
         this.setState({ visible }, () => resolve());
       });
 
-      startTries = 0;
+    startTries = 0;
 
-      mounted = false;
+    mounted = false;
 
-      eventEmitter = mitt();
+    eventEmitter = mitt();
 
-      isFirstStep = (): boolean => this.state.currentStep === this.getFirstStep();
+    isFirstStep = (): boolean => this.state.currentStep === this.getFirstStep();
 
-      isLastStep = (): boolean => this.state.currentStep === this.getLastStep();
+    isLastStep = (): boolean => this.state.currentStep === this.getLastStep();
 
-      registerStep = (step: Step): void => {
-        this.setState(({ steps }) => ({
-          steps: {
-            ...steps,
-            [step.name]: step,
-          },
-        }));
-      }
-
-      unregisterStep = (stepName: string): void => {
-        if (!this.mounted) {
-          return;
+    registerStep = (step: Step): void => {
+      this.setState(({ steps }) => ({
+        steps: {
+          ...steps,
+          [step.name]: step
         }
-        this.setState(({ steps }) => ({
-          steps: Object.entries(steps)
-            .filter(([key]) => key !== stepName)
-            .reduce((obj, [key, val]) => Object.assign(obj, { [key]: val }), {}),
-        }));
-      }
-
-      next = async (): void => {
-        await this.setCurrentStep(this.getNextStep());
-      }
-
-      prev = async (): void => {
-        await this.setCurrentStep(this.getPrevStep());
-      }
-
-      start = async (fromStep?: string, scrollView?: React.RefObject): void => {
-        const { steps } = this.state;
-
-        if (!this.state.scrollView) {
-          this.setState({ scrollView });
-        }
-
-        const currentStep = fromStep
-          ? steps[fromStep]
-          : this.getFirstStep();
-
-        if (this.startTries > MAX_START_TRIES) {
-          this.startTries = 0;
-          return;
-        }
-
-        if (!currentStep) {
-          this.startTries += 1;
-          requestAnimationFrame(() => this.start(fromStep));
-        } else {
-          this.eventEmitter.emit('start');
-          await this.setCurrentStep(currentStep);
-          await this.moveToCurrentStep();
-          await this.setVisibility(true);
-          this.startTries = 0;
-        }
-      }
-
-      stop = async (): void => {
-        await this.setVisibility(false);
-        this.eventEmitter.emit('stop');
-      }
-
-      async moveToCurrentStep(): void {
-        const size = await this.state.currentStep.target.measure();
-
-        await this.modal.animateMove({
-          width: size.width + OFFSET_WIDTH,
-          height: size.height + OFFSET_WIDTH,
-          left: size.x - (OFFSET_WIDTH / 2),
-          top: (size.y - (OFFSET_WIDTH / 2)) + verticalOffset,
-        });
-      }
-
-      render() {
-        return (
-          <View style={wrapperStyle || { flex: 1 }}>
-            <WrappedComponent
-              {...this.props}
-              start={this.start}
-              currentStep={this.state.currentStep}
-              visible={this.state.visible}
-              copilotEvents={this.eventEmitter}
-            />
-            <CopilotModal
-              next={this.next}
-              prev={this.prev}
-              stop={this.stop}
-              visible={this.state.visible}
-              isFirstStep={this.isFirstStep()}
-              isLastStep={this.isLastStep()}
-              currentStepNumber={this.getStepNumber()}
-              currentStep={this.state.currentStep}
-              labels={labels}
-              stepNumberComponent={stepNumberComponent}
-              tooltipComponent={tooltipComponent}
-              tooltipStyle={tooltipStyle}
-              overlay={overlay}
-              animated={animated}
-              androidStatusBarVisible={androidStatusBarVisible}
-              backdropColor={backdropColor}
-              svgMaskPath={svgMaskPath}
-              stopOnOutsideClick={stopOnOutsideClick}
-              ref={(modal) => { this.modal = modal; }}
-            />
-          </View>
-        );
-      }
-    }
-
-    Copilot.childContextTypes = {
-      _copilot: PropTypes.object.isRequired,
+      }));
     };
 
-    return hoistStatics(Copilot, WrappedComponent);
+    unregisterStep = (stepName: string): void => {
+      if (!this.mounted) {
+        return;
+      }
+      this.setState(({ steps }) => ({
+        steps: Object.entries(steps)
+          .filter(([key]) => key !== stepName)
+          .reduce((obj, [key, val]) => Object.assign(obj, { [key]: val }), {})
+      }));
+    };
+
+    next = async (): void => {
+      await this.setCurrentStep(this.getNextStep());
+    };
+
+    prev = async (): void => {
+      await this.setCurrentStep(this.getPrevStep());
+    };
+
+    start = async (fromStep?: string, scrollView?: React.RefObject): void => {
+      const { steps } = this.state;
+
+      if (!this.state.scrollView) {
+        this.setState({ scrollView });
+      }
+
+      const currentStep = fromStep ? steps[fromStep] : this.getFirstStep();
+
+      if (this.startTries > MAX_START_TRIES) {
+        this.startTries = 0;
+        return;
+      }
+
+      if (!currentStep) {
+        this.startTries += 1;
+        requestAnimationFrame(() => this.start(fromStep));
+      } else {
+        this.eventEmitter.emit("start");
+        await this.setCurrentStep(currentStep);
+        await this.moveToCurrentStep();
+        await this.setVisibility(true);
+        this.startTries = 0;
+      }
+    };
+
+    stop = async (): void => {
+      await this.setVisibility(false);
+      this.eventEmitter.emit("stop");
+    };
+
+    progress = async () => {
+      if (this.isLastStep()) {
+        await this.stop();
+      } else {
+        await this.next();
+      }
+    };
+
+    async moveToCurrentStep(): void {
+      this.size = await this.state.currentStep.target.measure();
+      await this.modal.animateMove({
+        width: this.size.width + OFFSET_WIDTH,
+        height: this.size.height + OFFSET_WIDTH,
+        left: this.size.x - OFFSET_WIDTH / 2,
+        top: this.size.y - OFFSET_WIDTH / 2 + verticalOffset
+      });
+    }
+
+    getChildContext(): { _copilot: CopilotContext } {
+      return {
+        _copilot: {
+          registerStep: this.registerStep,
+          unregisterStep: this.unregisterStep,
+          getCurrentStep: () => this.state.currentStep,
+          progress: this.progress
+        }
+      };
+    }
+
+    render() {
+      return (
+        <View
+          onLayout={this.handleLayoutChange}
+          style={wrapperStyle || { flex: 1 }}
+          {...this.panResponder.panHandlers}
+        >
+          <WrappedComponent
+            {...this.props}
+            start={this.start}
+            currentStep={this.state.currentStep}
+            visible={this.state.visible}
+            copilotEvents={this.eventEmitter}
+          />
+          <CopilotModal
+            next={this.next}
+            prev={this.prev}
+            stop={this.stop}
+            visible={this.state.visible}
+            isFirstStep={this.isFirstStep()}
+            isLastStep={this.isLastStep()}
+            currentStepNumber={this.getStepNumber()}
+            currentStep={this.state.currentStep}
+            labels={labels}
+            stepNumberComponent={stepNumberComponent}
+            tooltipComponent={tooltipComponent}
+            tooltipStyle={tooltipStyle}
+            overlay={overlay}
+            animated={animated}
+            androidStatusBarVisible={androidStatusBarVisible}
+            backdropColor={backdropColor}
+            svgMaskPath={svgMaskPath}
+            stopOnOutsideClick={stopOnOutsideClick}
+            ref={modal => {
+              this.modal = modal;
+            }}
+          />
+        </View>
+      );
+    }
+  }
+
+  Copilot.childContextTypes = {
+    _copilot: PropTypes.object.isRequired
   };
+
+  return hoistStatics(Copilot, WrappedComponent);
+};
 
 export default copilot;
